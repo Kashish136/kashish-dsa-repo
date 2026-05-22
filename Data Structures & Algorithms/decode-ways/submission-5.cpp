d3250eb622e0d355dfbaa61342d714b01e04eb61
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-05-22 16:57:03 +0530

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+   int helper(string s , int index){
+
+       if(index == s.size()){
+         return 1 ;
+       }
+
+
+       int result1 = 0;
+
+       result1 = helper(s , index+1);
+
+       int result2 = 0;
+
+       if(index < s.size()-1){
+         
+         if(s[index] == '1'  || (s[index] == '2' && s[index+1] < '7')){
+            result2 += helper(s , index+2);
+         }
+       }
+
+     return (result1 + result2);
+   }
+    int numDecodings(string s) {
+        
+        return helper(s,0);
+    }
+};
