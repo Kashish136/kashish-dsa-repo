ab13b43a0a4aa46db84c814fa3f0f14f81430676
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-05-26 23:07:29 +0530

    Add: delete-node-in-a-bst - submission-2

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-2.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+   TreeNode*helper(TreeNode*root){
+
+      TreeNode*current = root;
+    while(root->left != nullptr){
+        root = root->left;
+    }
+
+     return root ;
+   }
+
+   // my secret for finding out the smallest value in the right subtree
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        
+    if(!root){
+        return nullptr;
+    }
+
+    if(root->val > key){
+        return deleteNode(root->left , key);
+    }
+
+    else if(root->val < key){
+        return deleteNode(root->right , key);
+    }
+
+
+    else{
+
+    
+
+
+
+        if(!root->left && !root->right && root->val == key){
+            delete root;
+            return nullptr ;
+        }
+        else if(root->left != nullptr){
+            TreeNode*temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        else if(root->right != nullptr){
+            TreeNode*temp = root->right;
+            delete root ;
+            return temp ;
+        }
+
+        else{
+
+            TreeNode*newnode = helper(root->right , key);
+            root->val = newnode->val;
+            root->right = deleteNode(root->right , key);
+
+        }
+
+    }
+    }
+};
