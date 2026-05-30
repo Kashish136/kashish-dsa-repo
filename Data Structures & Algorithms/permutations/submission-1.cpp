73b7e0c19c2bd3dab00a780b796b3550c8902c8b
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-05-30 20:24:55 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:  
+
+    void helper( int index , vector<int>&nums , int n , vector<vector<int>>&output ){
+
+    if(index == nums.size()){
+        output.push_back(nums);
+        return ;
+    }
+
+    for( int i = index ; i < n ; i++){
+
+        swap(nums[i] , nums[index]);
+        
+        helper(index+1 , nums , n , output);
+        swap(nums[i] , nums[index]);
+
+    }
+
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>>output ;
+        int n = nums.size();
+        helper(0 , nums , n , output);
+    }
+};
