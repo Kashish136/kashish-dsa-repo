48f16e4ccdbec6b69e761194526f0de403fcf3d0
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-01 23:43:47 +0530

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        
+        // sort(intervals.begin() , intervals.end() , [](const Interval&a , 
+        // Interval &b){
+
+        //     return a.start < b.start;
+        // });
+
+
+        // priority_queue<int , vector<int> , greater<int>>pq;
+        // // we are declaring a minheap;
+        // for(const auto&it : intervals){
+        //     if(!pq.empty() && pq.top() <= it.start){
+        //         pq.pop();
+        //     }
+        //     pq.push(it.end);
+        // }
+         
+        //  return pq.size();
+
+        vector<int>start , end;
+        for( auto it : intervals){
+            start.push_back(it.start);
+            end.push_back(it.end);
+        }
+
+        sort(start.begin() ,start.end());
+        sort(end.begin() , end.end());
+
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        int n = intervals.size();
+        int output = 0;
+
+        while( i < n ){
+            if(start[i] < end[j]){
+                i++;
+                ans++;
+            }
+            else{
+                j++;
+                ans--;
+            }
+            output = max(output  , ans);
+        }
+
+        return output ;
+    }
+};
