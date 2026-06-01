6d00077e59039eb2fe0323fa441142d364b07a81
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-01 23:08:14 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        
+        sort(intervals.begin() , intervals.end() , [](const interval&a , 
+        interval &b){
+
+            return a.start < b.start;
+        });
+
+
+        priority_queue<int , vector<int> , greater<int>>pq;
+        // we are declaring a minheap;
+        for(const auto&it : intervals){
+            if(!pq.empty() && pq.top() <= it.start){
+                pq.pop();
+            }
+            pq.push(it.end);
+        }
+         
+         return pq.size();
+    }
+};
