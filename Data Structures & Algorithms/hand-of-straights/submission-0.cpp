1a007145c66bfdbefca9223584d02cdddeb4a701
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-01 15:12:43 +0530

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        
+
+        sort(hand.begin() , hand.end());
+        int n = hand.size();
+        map<int,int>mpp;
+
+        for(int i = 0 ; i < n ; i++){
+            int value = hand[i];
+            mpp[value]++;
+        }
+
+        // making the map for keeping the elements in sorted fashion 
+
+
+
+        while(true){
+            
+
+            if(mpp.empty()){
+                return true ;
+            }
+
+
+            int mini = min_element(hand.begin() , hand.end());
+            if(mpp[mini] > 0){
+
+                for(int i = 0 ; i < 4 ; i++){
+
+                    if(mpp[mini+i] > 0){
+                        mpp[mini]--;
+
+                        if(mpp[mini] == 0){
+                            mpp.erase(mini);
+                        }
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+
+        return false ;
+    }
+};
