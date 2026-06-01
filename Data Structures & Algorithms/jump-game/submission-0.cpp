d334ccacc9eac20dedc29bec1f5c94793812e662
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-01 23:56:15 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int maxposition = 0;
+
+        for( int i = 0 ; i < nums.size() ; i++){
+
+            if( i > maxposition){
+                return false;
+            }
+
+            maxposition = max(maxposition , (i+nums[i]));
+        }
+
+        return true ;
+    }
+};
