df482e154038c34446a6af4daf9d49297e5ba305
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-02 16:12:14 +0530

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public: 
+    
+    int helper(vector<int>&nums , int index){
+    if( index >= nums.size()){
+        return 0 ;
+    }
+
+    // if we have reached the final index we must return 0 as an answer 
+
+    int value = INT_MAX;
+    
+    int key = min((int)nums.size()-1 , (index+nums[index]));
+
+    for( int i = index+1 ; i <= key ; i++){
+        value = min(value , (1+helper(nums , i)))
+    }
+
+    return value;
+}
+
+
+    int jump(vector<int>& nums) {
+        return helper(nums , 0);
+    }
+};
