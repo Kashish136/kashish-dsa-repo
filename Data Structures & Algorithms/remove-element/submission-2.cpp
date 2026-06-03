4eb27c6f6f08eca5d7dfbad09946239013354a5f
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-03 15:01:44 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int i = 0;
+        vector<int>output;
+
+        for( int j = 0 ; j < nums.size() ; j++){
+            if(nums[j] == val){
+                continue;
+            }
+            else{
+                output[i] = nums[j];
+                i++;
+            }
+        }
+
+        return output.size();
+    }
+};
