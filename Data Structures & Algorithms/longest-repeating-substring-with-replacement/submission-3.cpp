6a64eb8e0990dfd3ab52e411fb96e56505294927
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-05 19:15:30 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        
+        unordered_map<char , int>mpp;
+        int left = 0;
+        int maxfreq = 0;
+
+        for(int right = 0 ; right < s.size() ; right++){
+             char ch = s[right];
+             mpp[ch]++;
+             maxfreq = max(maxfreq , mpp[ch]);
+
+        // jo bhi total length hai usmai jo bhi character
+        // sabse baar baar repeat horha hai hum sirf usko consider 
+        // krenge for solving this problem if agr uska count k 
+        // se bda hojayega toh hum eliminate krdenge 
+        while((right-left+1)-maxfreq > k){
+           mpp[s[left]]--;
+           left++;
+        }
+
+        output = max(output , right-left+1);
+
+
+         
+        }
+
+        return output ;
+    }
+};
