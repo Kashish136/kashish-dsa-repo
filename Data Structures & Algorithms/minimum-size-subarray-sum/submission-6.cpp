f1c2def0ba99c46f58bf2937e4e1826b31afc3cd
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-06 19:12:38 +0530

    Add: minimum-size-subarray-sum - submission-6

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+        int left = 0;
+        int output = INT_MAX;
+        int sum = 0;
+        
+
+        for(int right = 0 ; right < nums.size() ; right++){
+            int num = nums[i];
+            sum += num;
+
+            while(left <= right && sum >= target){
+                sum -= nums[left];
+                left++;
+                output = min(output , (right-left+1));
+            }
+
+        }
+
+        return output == INT_MAX ? 0 : output ;
+    }
+};
