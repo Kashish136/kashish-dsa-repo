f821a2319b38ad71a67c3c877206cf36879fef8e
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-07 23:51:07 +0530

    Add: split-array-largest-sum - submission-0

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+ 
+
+   bool helper(vector<int>&nums , int k , int mid){
+     
+     int count = 1;
+     int allocated = 0;
+
+     for(int i = 0 ; i < nums.size() ; i++){
+        if((allocated + nums[i]) <= mid){
+            allocated += nums[i];
+        }
+        else{
+            count++;
+            allocated = nums[i];
+        }
+     }
+
+     return (count <= k);
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        
+        int low = max_element(nums.begin() , nums.end());
+        int high = accumualte(nums.begin(), nums.end() , 0);
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(helper(nums , k , mid)){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        return low;
+    }
+};
