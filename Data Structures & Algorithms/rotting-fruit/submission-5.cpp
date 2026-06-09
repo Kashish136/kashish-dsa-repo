5a96a1056b70af8a5ea758ca802910aca046dc8c
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-09 22:52:08 +0530

    Add: rotting-fruit - submission-5

---
+++ Data Structures & Algorithms/rotting-fruit/submission-5.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+       vector<int>rows = {-1,1,0,0};
+       vector<int>cols = {0,0,-1,1};
+
+       int n = grid.size();
+       int m = grid[0].size();
+
+       queue<pair<pair<int,int>,int>>q;
+
+       for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            if(grid[i][j] == 2){
+                q.push({0 , {i,j}});
+                visited[i][j] = 1;
+            }
+        }
+       }
+
+       vector<vector<int>>visited(n , vector<int>(m,0));
+
+
+       int output = 0;
+       while(!q.empty()){
+         int time = q.front().second;
+         int row = q.front().first.first;
+         int col = q.front().first.second;
+         q.pop();
+         output = max(output , time);
+
+         for(int i = 0 ; i < 4 ; i++){
+            int nrow = row + rows[i];
+            int ncol = col + cols[i];
+
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+            grid[nrow][ncol] == 1 && !visited[nrow][ncol]){
+                grid[nrow][ncol] = 2;
+                visited[nrow][ncol] = 1;
+                q.push({{nrow , ncol} , time+1});
+            }
+         }
+       }
+
+       for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            if(grid[i][j] == 1){
+                return -1;
+            }
+        }
+       }
+
+       return output ;
+    }
+};
