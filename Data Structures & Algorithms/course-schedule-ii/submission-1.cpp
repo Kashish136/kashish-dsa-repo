0a3e16e750f44634f46f4a506515096e3982d656
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-11 21:55:09 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+
+        vector<vector<int>>adj(numCourses);
+
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].push_back(u);
+        }
+        
+        vector<int>indegree(n,0);
+        for(int i = 0 ; i < numCourses ; i++){
+           for( auto it : adj[i]){
+            indegree[it]++;
+           }
+        }
+
+
+        queue<int>q;
+        vector<int>topo;
+        for( int i = 0 ; i < numCourses ; i++){
+            if(indegee[i] == 0){
+                q.push(i);
+            }
+        }
+
+
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            topo.push_back(it);
+
+
+            for( auto ex : adj[it]){
+                indegree[ex]--;
+                if(indegree[ex] == 0){
+                    q.push(i);
+                }
+            }
+        }
+
+
+        if(topo.size() == numCourses){
+            return topo;
+        }
+
+        return {};
+    }
+};
