596813b863815dfbed738ddff3e627c62c529bd7
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-17 17:01:39 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char>st;
+
+        for(char ch : s){
+
+            if( ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+
+            else{
+
+                if(st.empty()){
+                    return false;
+                }
+
+                if((ch == ')' && st.top() == '(')
+                || (ch == '}' && st.top() == '{') || 
+                (ch == ']' && st.top() == '[')){
+                    st.pop();
+                }
+
+                else{
+                    st.push(ch);
+                }
+            }
+        }
+
+            return st.empty();
+    
+    }
+};
