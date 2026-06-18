ac48e14d6aba87fda4051d9b88be3d35ec32ae4f
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-06-18 21:42:15 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   
+   int helper(int n ){
+
+    int count = 0;
+    while( n > 0){
+        n = n&(n-1);
+        count++;
+    }
+
+    return count;
+   }
+    vector<int> countBits(int n) {
+        
+    vector<int>ans;
+    for(int i = 0 ; i <= n ; i++){
+        int key = helper(i);
+        ans.push_back(key);
+    }
+
+    return ans ;
+    }
+};
