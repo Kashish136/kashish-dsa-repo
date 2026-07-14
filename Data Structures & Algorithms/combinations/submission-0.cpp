5e37fcba2f8931aad6a368be7239079f6af587ab
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-07-14 14:04:06 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  
+void helper(int index , int n , int k , vector<vector<int>>&output ,
+ vector<int>&ans){
+
+    if( ans.size() == k ){
+       output.push_back(ans);
+       return;
+    }
+
+   
+
+    for(int i = index ; i <= n ; i++){
+     ans.push_back(i);
+     helper(i+1 , n , k , output , ans);
+     ans.pop_back();
+
+    }
+
+}
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<vector<int>>output;
+        vector<int>ans;
+        return helper(1 , n , k , output , ans);
+    }
+};
