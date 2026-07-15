09eaee9d64237ca7afb467f3e8c861cc60d42328
kashish <kashish.tiwari.ug23@nsut.ac.in>
2026-07-15 15:46:20 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for(int i = 0 ; i < bills.size() ; i++){
+            int bill = bills[i];
+            if(bill == 5){
+                five++;
+            }
+
+            else if(bill == 10){
+                ten++;
+                if(five > 0){
+                    five--;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            else{
+                if(ten > 0 && five > 0){
+                    twenty++;
+                    ten--;
+                    five--;
+                }
+
+                else if(five >= 3){
+                    five--;
+                    five--;
+                    five--;
+                    twenty++;
+                }
+
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return true ;
+    }
+};
